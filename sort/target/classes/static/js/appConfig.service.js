cc432d3fa91bfaecf468f09b94e6b59735bdc90a
vigneshkumar50 <49348707+vigneshkumar50@users.noreply.github.com>
2019-04-09 17:48:54 +0530

    target and .project file removed

--- sort/target/classes/static/js/appConfig.service.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    var app = angular.module("SortApp");
-    app.service("AppDataServiceSvc", function (AppNameSvc) {
-        this.name = AppNameSvc;
-        this.author = "Vigneshkumar G";
-        this.builtDate = new Date().toDateString();
-    });
-})();
